61464382eca4a9210b1fe47c557cc38193b93311
neko-no <noran4ko@gmail.com>
2026-05-01 16:18:06 +0900

    feat: 値オブジェクトのテスト #3

--- CatalogService/src/Domain/models/Review/Rating/Rating.ts
+++ CatalogService/src/Domain/models/Review/Rating/Rating.ts
@@ -3,8 +3,22 @@ import { isEqual } from "lodash";
 export class Rating {
     private readonly _value: number;
 
+    static readonly MIN = 1;
+    static readonly MAX = 5;
+
     constructor(value: number) {
+        this.validate(value);
         this._value = value;
+    }
+
+    protected validate(value: number): void {
+        if (!Number.isInteger(value)) {
+            throw new Error("評価値は整数値出なければなりません。");
+        }
+
+        if (value < Rating.MIN || value > Rating.MAX) {
+            throw new Error(`評価値は${Rating.MIN}から${Rating.MAX}までの整数値でなければなりません。`);
+        }
     }
 
     equals(other: Rating): boolean {
@@ -13,5 +27,9 @@ export class Rating {
 
     get value(): number {
         return this._value;
+    }
+
+    getQualityFactor(): number {
+        return (this._value - Rating.MIN) / (Rating.MAX - Rating.MIN);
     }
 }

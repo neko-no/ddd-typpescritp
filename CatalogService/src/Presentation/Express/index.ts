025d5b94c140d74726ace0a8c47d0b323227ab5a
neko-no <noran4ko@gmail.com>
2026-05-04 10:39:24 +0900

    refactor: 確認用のエンドポイントを削除 #15

--- CatalogService/src/Presentation/Express/index.ts
+++ CatalogService/src/Presentation/Express/index.ts
@@ -144,10 +144,6 @@ app.delete("/review/:reviewId", async (req, res) => {
   }
 });
 
-app.get("/", (_, res) => {
-  res.send("Hello, World!");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });

91a17ca91cc5f6fa46943b4815dcb9eee62c4bd9
neko-no <noran4ko@gmail.com>
2026-05-05 15:51:24 +0900

    feat: ドメインイベントのインターフェイスの依存関係をDIコンテナで制御 #22

--- CatalogService/src/Program.ts
+++ CatalogService/src/Program.ts
@@ -3,6 +3,8 @@ import { container } from "tsyringe";
 import { SQLBookRepository } from "Infrastructure/SQL/Book/SQLBookRepository";
 import { SQLReviewRepository } from "Infrastructure/SQL/Review/SQLReviewRepository";
 import { SQLTransactionManager } from "Infrastructure/SQL/SQLTransactionManager";
+import { EventEmitterDomainEventPublisher } from "Infrastructure/EventEmitter/EventEmitterDomainEventPublisher";
+import { EventEmitterDomainEventSubscriber } from "Infrastructure/EventEmitter/EventEmitterDomainEventSubscriber";
 
 container.register("IBookRepository", {
   useClass: SQLBookRepository,
@@ -14,4 +16,13 @@ container.register("IReviewRepository", {
 
 container.register("ITransactionManager", {
   useClass: SQLTransactionManager,
+});
+
+// DomainEvent
+container.register("IDmainEvenPulisher", {
+  useClass: EventEmitterDomainEventPublisher,
+});
+
+container.register("IDomainEventSubscriber", {
+  useClass: EventEmitterDomainEventSubscriber,
 });
